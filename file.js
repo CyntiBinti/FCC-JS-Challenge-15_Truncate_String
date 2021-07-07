6a0d7ff8ef1868635c046a132516220f393d62ff
Cynthia Mbulu <72040367+CyntiBinti@users.noreply.github.com>
2021-07-07 02:01:14 +0100

    Update file.js

--- file.js
+++ file.js
@@ -1,7 +1,7 @@
 function truncateString(str, num) {
 let arr = str;
 let truncated = arr.slice(0, (num));
-console.log(truncated+'...');
+return truncated + '...';
 }
 
 truncateString("A-tisket a-tasket A green and yellow basket", 8);
